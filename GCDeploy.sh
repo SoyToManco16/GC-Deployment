1aa6052367b2049df61c0c717f89923f1fb50cb4
Maikel-HA17 <5815957@alu.murciaeduca.es>
2025-04-12 13:59:56 +0200

    Update GCDeploy.sh

--- GCDeploy.sh
+++ GCDeploy.sh
@@ -23,31 +23,36 @@ error()   { echo -e "\e[31m[ERROR]\e[0m $*"; }    # Rojo
 
 # Mostrar info por pantalla
 info "Bienvenido a GCDeploy"
-info "Cargando archivo de configuración"
+info "Cargando archivo de configuración\n"
 
 # Comprobar si el archivo de configuración existe
 if [[ -f ./gc_env.conf ]]; then
     source ./gc_env.conf
-    echo ""; ok "Variables de entorno configuradas correctamente"
+    ok "Variables de entorno configuradas correctamente\n"
 else
     error "El archivo no existe, abortando..."
     exit
 fi
 
 # Mostrar info del despliegue por pantalla
-echo ""
 info "Desplegando como: $OS_USERNAME"
-info "Nombre del proyecto: $OS_PROJECT_NAME"; echo ""
+info "Nombre del proyecto: $OS_PROJECT_NAME\n"
 
-info "Añadiendo hostname a /etc/hosts"
+# Añadir hostname si no está
+info "Añadiendo hostname a /etc/hosts\n"
 
 if ! grep -q "127.0.0.1  $HOSTNAME" /etc/hosts; then
   echo "127.0.0.1  $HOSTNAME" >> /etc/hosts
 fi
 
-info "Actualizando sistema e instalando dependencias"
+# Añadir repositorio de OpenStack
+info "Añadiendo repositorio de OpenStack"
+if ! grep -q "cloud-archive:caracal" /etc/apt/sources.list /etc/apt/sources.list.d/*; then
+  sudo add-apt-repository -y cloud-archive:caracal
+fi
 
 # Actualizar sistema
+info "Actualizando sistema e instalando dependencias\n"
 sudo apt update && sudo apt -y upgrade && sudo apt -y dist-upgrade
 
 # Instalar dependencias
@@ -65,34 +70,95 @@ sudo apt update && sudo apt install -y \
   crudini \
   chrony
 
-
 # Preparar chrony
-info "Configurando chrony"
-sudo systemctl enable --now chrony
-chronyc sources; echo ""
+info "Configurando chrony\n"
+if systemctl is-active --quiet chrony; then
+    ok "Chrony !!"
+    sudo systemctl enable --now chrony
+    chronyc sources; echo ""
+else 
+    error "Chrony no está funcionando"
+    exit 1 
+fi
+
+# Preparar cola de mensajería
+info "Configurando RabbitMQ (Cola de mensajería)"
+sudo systemctl enable --now rabbitmq-server
+sudo systemctl start rabbitmq-server
+
+if systemctl is-active --quiet rabbitmq-server; then
+    sudo rabbitmqctl add_user "$RABBIT_USER" "$RABBIT_PASS"
+    sudo rabbitmqctl set_permissions "$RABBIT_USER" ".*" ".*" ".*"
+
+    ok "RabbitMQ configurado con éxito"
+    info "Usuarios de RabbitMQ: $(sudo rabbitmqctl list_users)"
+    info "Permisos de los usuarios de RabbitMQ: $(sudo rabbitmqctl list_permissions)"
+else 
+    error "RabbitMQ no está funcionando"
+    exit 1
+fi
+
+
 
 # Preparar bases de datos
-info "Configurando BBDDs"
-sudo mysql -u root -p"$DB_ROOT_PASS" -h "$DB_HOST" <<EOF
+info "Configurando MariaDB"
 
-CREATE DATABASE IF NOT EXISTS $KEYSTONE_DB;
-CREATE DATABASE IF NOT EXISTS $GLANCE_DB;
-CREATE DATABASE IF NOT EXISTS $NOVA_DB;
-CREATE DATABASE IF NOT EXISTS $NEUTRON_DB;
-CREATE DATABASE IF NOT EXISTS $CINDER_DB;
-
-CREATE USER IF NOT EXISTS '$KEYSTONE_DBUSER'@'$HOSTNAME' IDENTIFIED BY '$KEYSTONE_DBPASS';
-CREATE USER IF NOT EXISTS '$GLANCE_DBUSER'@'$HOSTNAME' IDENTIFIED BY '$GLANCE_DBPASS';
-CREATE USER IF NOT EXISTS '$NOVA_DBUSER'@'$HOSTNAME' IDENTIFIED BY '$NOVA_DBPASS';
-CREATE USER IF NOT EXISTS '$NEUTRON_DBUSER'@'$HOSTNAME' IDENTIFIED BY '$NEUTRON_DBPASS';
-CREATE USER IF NOT EXISTS '$CINDER_DBUSER'@'$HOSTNAME' IDENTIFIED BY '$CINDER_DBPASS';
-
-GRANT ALL PRIVILEGES ON $KEYSTONE_DB.* TO '$KEYSTONE_DBUSER'@'$HOSTNAME';
-GRANT ALL PRIVILEGES ON $GLANCE_DB.* TO '$GLANCE_DBUSER'@'$HOSTNAME';
-GRANT ALL PRIVILEGES ON $NOVA_DB.* TO '$NOVA_DBUSER'@'$HOSTNAME';
-GRANT ALL PRIVILEGES ON $NEUTRON_DB.* TO '$NEUTRON_DBUSER'@'$HOSTNAME';
-GRANT ALL PRIVILEGES ON $CINDER_DB.* TO '$CINDER_DBUSER'@'$HOSTNAME';
-
-FLUSH PRIVILEGES;
+# Archivo de configuración de mariadb
+sudo tee /etc/mysql/mariadb.conf.d/99-openstack.cnf <<EOF
+[mysqld]
+bind-address = 0.0.0.0
+default-storage-engine = innodb
+innodb_file_per_table = on
+max_connections = 4096
+collation-server = utf8_general_ci
+character-set-server = utf8
 EOF
+
+sudo systemctl restart mariadb
+sudo systemctl enable --now mariadb
+
+if systemctl is-active --quiet mariadb; then
+    ok "MariaDB !!"
+
+    # Crear bases de datos y usuarios y permisos
+    sudo mysql -u root -p"$DB_ROOT_PASS" -h "$DB_HOST" <<EOF
+
+    CREATE DATABASE IF NOT EXISTS $KEYSTONE_DB;
+    CREATE DATABASE IF NOT EXISTS $GLANCE_DB;
+    CREATE DATABASE IF NOT EXISTS $NOVA_DB;
+    CREATE DATABASE IF NOT EXISTS $NEUTRON_DB;
+    CREATE DATABASE IF NOT EXISTS $CINDER_DB;
+
+    CREATE USER IF NOT EXISTS '$KEYSTONE_DBUSER'@'$HOSTNAME' IDENTIFIED BY '$KEYSTONE_DBPASS';
+    CREATE USER IF NOT EXISTS '$GLANCE_DBUSER'@'$HOSTNAME' IDENTIFIED BY '$GLANCE_DBPASS';
+    CREATE USER IF NOT EXISTS '$NOVA_DBUSER'@'$HOSTNAME' IDENTIFIED BY '$NOVA_DBPASS';
+    CREATE USER IF NOT EXISTS '$NEUTRON_DBUSER'@'$HOSTNAME' IDENTIFIED BY '$NEUTRON_DBPASS';
+    CREATE USER IF NOT EXISTS '$CINDER_DBUSER'@'$HOSTNAME' IDENTIFIED BY '$CINDER_DBPASS';
+
+    GRANT ALL PRIVILEGES ON $KEYSTONE_DB.* TO '$KEYSTONE_DBUSER'@'$HOSTNAME';
+    GRANT ALL PRIVILEGES ON $GLANCE_DB.* TO '$GLANCE_DBUSER'@'$HOSTNAME';
+    GRANT ALL PRIVILEGES ON $NOVA_DB.* TO '$NOVA_DBUSER'@'$HOSTNAME';
+    GRANT ALL PRIVILEGES ON $NEUTRON_DB.* TO '$NEUTRON_DBUSER'@'$HOSTNAME';
+    GRANT ALL PRIVILEGES ON $CINDER_DB.* TO '$CINDER_DBUSER'@'$HOSTNAME';
+
+    FLUSH PRIVILEGES;
+EOF
+else 
+    error "MariaDB no está funcionando"
+    exit 1
+fi
+
+# Preparar memcached
+info "Configurando memcached"
+if systemctl is-active --quiet memcached; then
+  ok "Memcached !!"
+  sudo sed -i 's/^# -l 127.0.0.1/-l 127.0.0.1/' /etc/memcached.conf
+  sudo systemctl enable memcached
+  sudo systemctl restart memcached
+else
+  error "Memcached no está funcionando"
+  exit 1
+fi
+
+
 
