a7904f25f7765bee2e83a064eacf8df0412691d1
Maikel-HA17 <5815957@alu.murciaeduca.es>
2025-04-13 12:59:08 +0200

    Update gen_sudo_usr.sh

--- gen_sudo_usr.sh
+++ gen_sudo_usr.sh
@@ -1,15 +1,25 @@
 #!/bin/bash
+
 # Comprobar que se pasa un argumento
 if [ -z "$1" ]; then
     echo -e "Error: Debes introducir un nombre de usuario."
     exit 1
 fi
 
-# Añadir usuario
-sudo adduser "$1"
+# Crear usuario con `useradd`
+sudo useradd -m -s /bin/bash "$1"
+
+# Asignar una contraseña (puedes modificar esto según lo necesites)
+echo "$1:password" | sudo chpasswd
+
+# Añadir al grupo sudo
 sudo usermod -aG sudo "$1"
 
 # Modificar sudoers con visudo
 echo "$1 ALL=(ALL) NOPASSWD:ALL" | sudo tee -a /etc/sudoers.d/$1 > /dev/null
 
-echo -e "Usuario '$1' creado, añadido al grupo sudo y configurado para usar sudo sin contraseña."
+# Contraseña
+sudo passwd "$1"
+
+# Exito
+echo "El usuario $1 ha sido añadido al grupo sudoers"
